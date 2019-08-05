92d0be29e42f206fcf01bfede1f28dc00f1bc7bd
VBTX <jv.svedaite@gmail.com>
2019-08-05 13:11:08 -0500

    add from_omniauth method for finding users via FB authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,29 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable,  :omniauth_providers => [:facebook]
   validates :fullname, presence: true, length: {maximum: 50}
+
+
+    def self.from_omniauth(auth)
+      user = User.where(email: auth.info.email).first 
+      if user
+      return user
+      else	
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      	user.provider = auth.provider
+      	user.uid = auth.uid
+        user.fullname = auth.info.name
+        user.email = auth.info.email
+        user.image = auth.info.image
+        user.password = Devise.friendly_token[0,10]
+      end
+    end
 end
+end
+
+
+# we have self in the method because we want to use it without initializing a new instance of User
+
+# we check if we got that email in our database or if not, we create it
