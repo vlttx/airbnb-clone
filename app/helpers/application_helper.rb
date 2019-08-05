0fbbd43f0092ea018a809a4cb33ea47e908a4a58
VBTX <jv.svedaite@gmail.com>
2019-08-05 13:20:57 -0500

    add possibility of adding pic from your account

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,11 @@ module ApplicationHelper
 
 	def avatar_url
 
+		if current_user.image?
+			current_user.image
+		else
 		"https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
-
+	end
 
 		# gravatar_id = Digest::MD5::hexdigest(user.email).downcase
 		# "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
