7124418d2842636483d882bdb7d855d9f340384c
VBTX <jv.svedaite@gmail.com>
2019-08-05 15:03:45 -0500

    add validations to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,12 @@
 class Room < ApplicationRecord
   belongs_to :user
+
+  validates :home_type, presence: true
+  validates :room_type, presence: true
+  validates :accommodates, presence: true
+  validates :bedroom, presence: true
+  validates :bathroom, presence: true
+  validates :listing_name, presence: true, length: {maximum: 50}
+  validates :summary, presence: true, length: {maximum: 500}
+  validates :address, presence: true
 end
