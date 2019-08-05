c683fffb89f723fdde2adff984978a3c2892eb16
VBTX <jv.svedaite@gmail.com>
2019-08-05 13:08:46 -0500

    add routes to login and signin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users, 
+  			 :path => '', 
+  			 :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+  			 :controllers => {:omniauth_callbacks  => 'callbacks'}
+  			 # calling omniauth controller
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
