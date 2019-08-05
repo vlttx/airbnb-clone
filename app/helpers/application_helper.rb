f71a33d699877a129af57876bddbff4e5e333209
VBTX <jv.svedaite@gmail.com>
2019-08-05 14:55:48 -0500

    change default pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 		if current_user.image?
 			current_user.image
 		else
-		"https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
+		"https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000101//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100"
 	end
 
 		# gravatar_id = Digest::MD5::hexdigest(user.email).downcase
