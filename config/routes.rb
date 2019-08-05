d635ecc60ab029a5355246492bc7723ad8e2a6e4
VBTX <jv.svedaite@gmail.com>
2019-08-05 15:16:42 -0500

    add routes for rooms

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   			 # calling omniauth controller
 
   resources :users, only: [:show]
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
