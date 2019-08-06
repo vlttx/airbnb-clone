ed941227d1703834c9ba00ccc94b566bc2ad1ccd
VBTX <jv.svedaite@gmail.com>
2019-08-06 16:35:05 -0500

    add routes for rooms

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :rooms
+  resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
