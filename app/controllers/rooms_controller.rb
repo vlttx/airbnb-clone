85a973d609a787dba9b5a9118af845e832aefb5a
VBTX <jv.svedaite@gmail.com>
2019-08-06 16:45:22 -0500

    add show, edit, create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,13 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
+
   def index
     @rooms = current_user.room
   end
 
   def show
-
+      @photos = @room.photos
   end
 
   def new
@@ -17,18 +18,38 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.build(room_params)
 
     if @room.save
-      redirect_to @room, notice: "Saved"
+
+      if params[:images]
+        # if we can find images, then we'll look through the list and create photos for the room
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      @photos = @room.photos
+      # we are going to display a list of photos, that is why it is necessary
+      redirect_to edit_room_path@room), notice: "Saved"
     else
       render :new
     end
   end
 
   def edit
+    if current_user.id == @room.user_id
+      @photos = @room.photos
+    else
+      redirect_to root_path, notice: "You don't have permission."
+    end
   end
 
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "Updated"
+      if params[:images]
+        # if we can find images, then we'll look through the list and create photos for the room
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      redirect_to edit_room_path@room), notice: "Updated"
     else
       render :edit
     end
