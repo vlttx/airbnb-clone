511792d502223c3936176bbde98d0acc857ab166
VBTX <jv.svedaite@gmail.com>
2019-08-18 20:29:20 -0500

    finish search functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,25 +6,28 @@ class PagesController < ApplicationController
   def search
   	if params[:search].present? && params[:search].strip != ""
   		session[:loc_search] = params[:search]
-  	else
-  		session[:loc_search] - ""
+      # we check if the user provided location or not. We use session variable
+      # in order to remember the location they search so that they would not need to retype
   	end
 
   	arrResult = Array.new
 
   	if session[:loc_search] && session[:loc_search] != ""
   		@rooms_address = Room.where(active: true).near(session[:loc_search], 5, order: 'distance')
+      # if user entered a location, we provide rooms nearby
   	else
   		@rooms_address = Room.where(active: true).all
   	end
 
   	@search = @rooms_address.ransack(params[:q])
+    # params q comes from search view where we have Q for entries (standard for ransack)
   	@rooms = @search.result
   	# these methods come from ransack. We'll get all the rooms into the variable
+    # that meet all user selected features
 
   	@arrRooms = @rooms.to_a 
   	# we need to go through those room and that is why we get them into an array
-
+    # below we search for a specific date range and see if that range involves unavailable rooms, so we can remove it from results
   	if (params[:start_date] && params[:end_date] && !params[:start_date].empty? && !params[:end_date].empty?)
   		start_date = Date.parse(params[:start_date])
   		end_date = Date.parse(params[:end_date])
@@ -50,3 +53,5 @@ class PagesController < ApplicationController
 end
 
 # strip method removes all blank spaces or empty; loc_search is a name of our choosing
+
+# 
