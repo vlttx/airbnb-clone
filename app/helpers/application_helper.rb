c46189659342e67b7c99136a1a3acd3db5bc0ef5
VBTX <jv.svedaite@gmail.com>
2019-08-20 16:10:13 -0500

    add user arg to avatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
 
-	def avatar_url
-
-		if current_user.image?
-			current_user.image
+	def avatar_url(id)
+		user = User.all.find_by(id: id)
+		if user.image?
+			user.image
 		else
-		"https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000101//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100"
+		 "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+		# "https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000101//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100"
 	end
 
 		# gravatar_id = Digest::MD5::hexdigest(user.email).downcase
