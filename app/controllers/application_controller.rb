81d5501b0e8487b55587790df4fb44ff465dc34d
VBTX <jv.svedaite@gmail.com>
2019-08-20 20:05:38 -0500

    add avatar into permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :email])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :avatar])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :email, :avatar])
 	end
 
 	def after_sign_in_path_for(resource)
