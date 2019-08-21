7192827969911c13c8cf33c827ac73bdf7c9b5c8
VBTX <jv.svedaite@gmail.com>
2019-08-20 20:06:58 -0500

    add possibility to add user profile picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_193321) do
+ActiveRecord::Schema.define(version: 2019_08_21_003043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,10 @@ ActiveRecord::Schema.define(version: 2019_08_17_193321) do
     t.string "image"
     t.string "phone_number"
     t.text "description"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
